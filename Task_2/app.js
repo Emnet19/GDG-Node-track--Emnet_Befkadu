25b31e64ea025564e3ea27277e16823d29ba3175
Emnet Befkadu <emnbef456@gmail.com>
2025-12-16 19:31:14 +0300

    Adding style in the home route

--- Task_2/app.js
+++ Task_2/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app=express();
-const PORT =3000;
+const PORT =5000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on prot ${PORT}`);
@@ -10,15 +10,17 @@ app.listen(PORT,()=>{
 
 
   app.get('/home',(req,res)=>{
-    res.status(200).type('text/html').end('<h1>Welcome To The Home Page</h> ',style=color:green)
+    res.status(200).type('text/html').end('<h1 style="color:green;font-size:50px ;">Welcome To The Home Page</h1> ' )
   })
   app.get('/about',(req,res)=>{
     res.status(200).type('text/plain').end('This is the About Page' )
   })
 
   app.get('/students/:studentId',(req,res)=>{
-    const department= req.queries.department;
+    const department= req.query.department;
     const studentId=req.params.studentId;
-res.status(200).type('application/json').json({studentId
-    ,department
-})})
+res.status(200).type('application/json').json({
+    studentId,
+    department})
+
+})
